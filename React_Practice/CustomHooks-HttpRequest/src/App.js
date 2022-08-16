effbf6dbd0aa2a71495d8024995186cee626421c
mohammadali-amd <amidi1380@gmail.com>
2022-08-16 16:47:43 +0430

    Tasks app
    Custom Hooks
    Firebase Realtime DB
    Http Request ('GET' & 'Post')
    useCallback

---
+++ React_Practice/CustomHooks-HttpRequest/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp()
+
+  useEffect(() => {
+    const transFormTasks = (tasksObj) => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://react-http-ffb8b-default-rtdb.firebaseio.com/tasks.json' },
+      transFormTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
