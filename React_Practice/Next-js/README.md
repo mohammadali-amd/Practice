5550e0ff970d1673a8b04bf8baf1faa23ebbf100
mohammadali-amd <amidi1380@gmail.com>
2022-09-02 23:12:34 +0430

    Next-js

---
+++ React_Practice/Next-js/README.md
@@ -0,0 +1,5 @@
+<img src="public/next-meetups.png" alt="Next Meetups" title="Next Meetups" >
+
+<br /> <br />
+
+<img src="public/add-new-meetup.png" alt="Add New Meetup" title="Add New Meetup" >
---
+++ React_Practice/Next-js/components/meetups/MeetupDetail.js
@@ -0,0 +1,19 @@
+import classes from './MeetupDetail.module.css';
+
+function MeetupDetail(props) {
+  return (
+    <section className={classes.detail}>
+      <picture>
+        <img
+          src={props.image}
+          alt={props.title}
+        />
+      </picture>
+      <h1>{props.title}</h1>
+      <address>{props.address}</address>
+      <p>{props.description}</p>
+    </section>
+  );
+}
+
+export default MeetupDetail;
